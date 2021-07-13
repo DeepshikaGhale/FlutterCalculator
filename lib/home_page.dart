0bc7a850e5ef49fa2bc736a8662434ccc07a98e1
Deepshika Ghale <deepshikaghale99@gmail.com>
2021-07-13 14:23:25 +0545

    final commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,11 +6,61 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
-  var num1, num2, num3;
+  var num1 = 0, num2 = 0, num3 = 0;
+
+  final TextEditingController t1 = new TextEditingController();
+  final TextEditingController t2 = new TextEditingController();
 
   void doAddition() {
     setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+
       num3 = num1 + num2;
+    });
+  }
+
+  void doSubstraction() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+
+      num3 = num1 - num2;
+    });
+  }
+
+  void doMultiplication() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+
+      num3 = num1 * num2;
+    });
+  }
+
+  void doDivision() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+
+      num3 = num1 ~/ num2;
+    });
+  }
+
+  void checkModulus() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+
+      num3 = num1 % num2;
+    });
+  }
+
+  void Clear() {
+    setState(() {
+      t1.text = '0';
+      t2.text = '0';
+      num3 = 0;
     });
   }
 
@@ -38,10 +88,12 @@ class HomePageState extends State<HomePage> {
               new TextField(
                 keyboardType: TextInputType.number,
                 decoration: new InputDecoration(hintText: 'Enter Number 1'),
+                controller: t1,
               ),
               new TextField(
                 keyboardType: TextInputType.number,
                 decoration: new InputDecoration(hintText: 'Enter Number 2'),
+                controller: t2,
               ),
               new Padding(padding: EdgeInsets.only(top: 20.0)),
               new Row(
@@ -49,11 +101,11 @@ class HomePageState extends State<HomePage> {
                 children: [
                   new ElevatedButton(
                     child: new Text("+"),
-                    onPressed: () {},
+                    onPressed: doAddition,
                   ),
                   new ElevatedButton(
                     child: new Text("-"),
-                    onPressed: () {},
+                    onPressed: doSubstraction,
                   ),
                 ],
               ),
@@ -63,12 +115,21 @@ class HomePageState extends State<HomePage> {
                 children: [
                   new ElevatedButton(
                     child: new Text("*"),
-                    onPressed: () {},
+                    onPressed: doMultiplication,
                   ),
                   new ElevatedButton(
                     child: new Text("/"),
-                    onPressed: () {},
+                    onPressed: doDivision,
                   ),
+                ],
+              ),
+              new Padding(padding: EdgeInsets.only(top: 20.0)),
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  new ElevatedButton(
+                      onPressed: checkModulus, child: new Text('%')),
+                  new ElevatedButton(onPressed: Clear, child: new Text('Clear'))
                 ],
               )
             ],
